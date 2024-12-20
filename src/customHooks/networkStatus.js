bfd9a2f2516c4e8d48c48142eefee804ff9772a8
naveen <chennamsettynaveen945@gmail.com>
2024-12-20 13:44:20 +0530

    online and offline changes

--- src/customHooks/networkStatus.js
+++ src/customHooks/networkStatus.js
@@ -1,20 +1,14 @@
 import { useState, useEffect } from 'react';
 import NetInfo from '@react-native-community/netinfo';
 
-/**
- * Custom hook to monitor network online/offline status
- * @returns {boolean} - True if the device is online, false otherwise
- */
 const useNetworkStatus = () => {
-  const [isOnline, setIsOnline] = useState(true);  // Default state to online
+  const [isOnline, setIsOnline] = useState(true); 
 
   useEffect(() => {
-    // Function to handle online status
     const unsubscribe = NetInfo.addEventListener(state => {
-      setIsOnline(state.isConnected);  // Update the state based on the network status
+      setIsOnline(state.isConnected);
     });
 
-    // Cleanup function to remove the listener when component unmounts
     return () => unsubscribe();
   }, []);
 
